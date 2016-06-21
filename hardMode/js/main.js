b8f7876be3a5542c1e2413e23581baed43297d49
humdrum <jjrajani@gmail.com>
2016-06-20 22:47:24 -0400

    replaced gh-pages js with master js

--- hardMode/js/main.js
+++ hardMode/js/main.js
@@ -1,33 +1,3 @@
-// //create div class=container
-// //create h1 class=clock
-
-// var clock = document.getElementById("time")
-// clock.innerHTML = "BOO"
-// color = "000000"
-
-// var changeColor = function() {
-//   color = String((Number(color) + 1))
-//   while (color.length < 6) {
-//     color = 0 + color
-//   }
-//   if (color == "1000000") {
-//     color = "000000"
-//   }
-//   clock.innerHTML = ("#" + color)
-//   document.querySelector("body").style.backgroundColor = ("#" + color)
-// }
-
-// window.setInterval(changeColor, 1000);
-
-// var changeColor = function() {
-
-// }
-
-
-
-
-
-
 var background = document.querySelector("body");
 var changeColor = function() {
   
@@ -49,39 +19,3 @@ var changeColor = function() {
 
 
 window.setInterval(changeColor, 1000);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
