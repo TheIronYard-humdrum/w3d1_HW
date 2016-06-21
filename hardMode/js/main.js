b1b7e8676fd3b81a7c0fe3408b040cc366b8ef1c
humdrum <jjrajani@gmail.com>
2016-06-20 22:49:14 -0400

    final commit

--- hardMode/js/main.js
+++ hardMode/js/main.js
@@ -1,20 +1,21 @@
-//create div class=container
-//create h1 class=clock
-
-var clock = document.getElementById("time")
-clock.innerHTML = "BOO"
-color = "000000"
-
+var background = document.querySelector("body");
 var changeColor = function() {
-  color = String((Number(color) + 1))
-  while (color.length < 6) {
-    color = 0 + color
-  }
-  if (color == "1000000") {
-    color = "000000"
-  }
-  clock.innerHTML = ("#" + color)
-  document.querySelector("body").style.backgroundColor = ("#" + color)
+  
+  var now = new Date();
+  var hours = now.getHours();
+  var minutes = now.getMinutes();
+  var seconds = now.getSeconds();
+
+  hexhours = hours.toString(16);
+  hexmins = minutes.toString(16);
+  hexsecs = seconds.toString(16);
+
+  var clock = document.getElementById("time");
+  var clockTime = hexhours + hexmins + hexsecs
+  clock.innerHTML = clockTime;
+  background.style.backgroundColor = "#" + hexhours + hexmins + hexsecs;
 }
+
+
 
 window.setInterval(changeColor, 1000);
