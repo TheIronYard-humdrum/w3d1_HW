8ba7dae88e5ca4247c7641b70dbcf1effeab5d60
humdrum <jjrajani@gmail.com>
2016-06-21 08:41:41 -0400

    fixed glitch in color code single digits

--- hardMode/js/main.js
+++ hardMode/js/main.js
@@ -1,4 +1,5 @@
 var background = document.querySelector("body");
+var clock = document.getElementById("time");
 
 var changeColor = function() {
   
@@ -8,15 +9,45 @@ var changeColor = function() {
   var seconds = now.getSeconds();
 
   hexhours = hours.toString(16);
+  if (hexhours.length == 1) {
+    hexhours += hexhours
+  }
   hexmins = minutes.toString(16);
+  if (hexmins.length == 1) {
+    hexmins += hexmins
+  }
   hexsecs = seconds.toString(16);
+  if (hexsecs.length == 1) {
+    hexsecs += hexsecs
+  }
 
-  var clock = document.getElementById("time");
   var clockTime = hexhours + hexmins + hexsecs
   clock.innerHTML = clockTime;
-  background.style.backgroundColor = "#" + hexhours + hexmins + hexsecs;
+  background.style.backgroundColor = ("#" + hexhours + hexmins + hexsecs);
 }
 
 
 
 window.setInterval(changeColor, 1000);
+
+
+// var clock = document.getElementById("time");
+// var checkTime = function() {
+//   var now = new Date();
+//   var hours = now.getHours();
+//   if (String(hours).length == 1) {
+//     hours = "0" + hours
+//   }
+//   var minutes = now.getMinutes();
+//   if (String(minutes).length == 1) {
+//     minutes = "0" + minutes
+//   }
+//   var seconds = now.getSeconds();
+//   if (String(seconds).length == 1) {
+//   seconds = "0" + seconds
+// }
+//   var time = hours + ":" + minutes + ":" + seconds
+//   clock.innerHTML = time
+// }
+
+// window.setInterval(checkTime, 1000);
