2c9822f6c5877c6548c14d612087404cfd4a729e
humdrum <jjrajani@gmail.com>
2016-06-20 22:30:31 -0400

    fixed functionality. unmerged normal nad nightmare modes

--- hardMode/js/main.js
+++ hardMode/js/main.js
@@ -1,20 +1,87 @@
-//create div class=container
-//create h1 class=clock
+// //create div class=container
+// //create h1 class=clock
 
-var clock = document.getElementById("time")
-clock.innerHTML = "BOO"
-color = "000000"
+// var clock = document.getElementById("time")
+// clock.innerHTML = "BOO"
+// color = "000000"
 
+// var changeColor = function() {
+//   color = String((Number(color) + 1))
+//   while (color.length < 6) {
+//     color = 0 + color
+//   }
+//   if (color == "1000000") {
+//     color = "000000"
+//   }
+//   clock.innerHTML = ("#" + color)
+//   document.querySelector("body").style.backgroundColor = ("#" + color)
+// }
+
+// window.setInterval(changeColor, 1000);
+
+// var changeColor = function() {
+
+// }
+
+
+
+
+
+
+var background = document.querySelector("body");
 var changeColor = function() {
-  color = String((Number(color) + 1))
-  while (color.length < 6) {
-    color = 0 + color
-  }
-  if (color == "1000000") {
-    color = "000000"
-  }
-  clock.innerHTML = ("#" + color)
-  document.querySelector("body").style.backgroundColor = ("#" + color)
+  
+  var now = new Date();
+  var hours = now.getHours();
+  var minutes = now.getMinutes();
+  var seconds = now.getSeconds();
+
+  hexhours = hours.toString(16);
+  hexmins = minutes.toString(16);
+  hexsecs = seconds.toString(16);
+
+  var clock = document.getElementById("time");
+  var clockTime = "#" + hours + minutes + seconds
+  clock.innerHTML = clockTime;
+  background.style.backgroundColor = "#" + hexhours + hexmins + hexsecs;
 }
 
+
+
 window.setInterval(changeColor, 1000);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- nightmareMode/js/main.js
+++ nightmareMode/js/main.js
@@ -1,14 +1,21 @@
-//create div class=container
-//create h1 class=clock
-
-var clock = document.getElementById("time")
-
+var background = document.querySelector("body");
 var changeColor = function() {
-  var time = new Date();
-  time = Number(time).toString(16);
-  color = time.slice(5, 11)
-  clock.innerHTML = ("#" + color)
-  document.querySelector("body").style.backgroundColor = ("#" + color)
+  
+  var now = new Date();
+  var hours = now.getHours();
+  var minutes = now.getMinutes();
+  var seconds = now.getSeconds();
+
+  hexhours = hours.toString(16);
+  hexmins = minutes.toString(16);
+  hexsecs = seconds.toString(16);
+
+  var clock = document.getElementById("time");
+  var clockTime = hexhours + hexmins + hexsecs
+  clock.innerHTML = clockTime;
+  background.style.backgroundColor = "#" + hexhours + hexmins + hexsecs;
 }
+
+
 
 window.setInterval(changeColor, 1000);
